84b3baf4de05f8a65b97fbbbbdc8d2f1aa175539
Nuno Silva <nunosanta15@gmail.com>
2021-11-29 12:15:11 -0100

    primeiro comit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const path = require ('path')
+
+app.use(express.static('./public'))
+ 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,'/public/index.html'))
+}) 
+
+const port = 4000;
+
+app.listen(port, function (){
+  console.log("Listenning on port:" + port)
+})
