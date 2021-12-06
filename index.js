493135956beccdaaffc8a2bfbc6317bde3f187bf
NunoSilva21 <nunosanta15@gmail.com>
2021-12-06 10:03:11 -0100

    commit2

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const express = require('express')
-const path = require ('path')
+const path = require('path')
 const app = express()
-const connection = require ('./dbconnection')
+const connection = require('./dbconnection')
 app.use(express.static('./public'))
+
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/navbar',(req,res)=>{
+    res.sendFile(path.join(__dirname, './public/navbar.html'))
+})
+
+const port =3000
  
-app.get('/navbar', function (req, res) {
-  res.sendFile(path.join(__dirname, './public/navbar.html'))
-
-}) 
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './public/index.html'))
-
-}) 
-
-const port = 4000;
-
-app.listen(port, function (){
-  console.log("Listenning on port:" + port)
+app.listen(port, () => {
+    console.log(`Listenning on port ${port}`)
 })
